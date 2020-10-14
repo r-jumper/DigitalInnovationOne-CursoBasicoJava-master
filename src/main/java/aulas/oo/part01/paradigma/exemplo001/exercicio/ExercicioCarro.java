8c44d3fb1999472e6a5d7c87c037c87d3d4004be
Fernando Ramos <ramos.cesar.f@hotmail.com>
2020-10-14 15:55:03 -0300

    Adicionando arquivos da aula de OO com java e os arquivos do exercicio proposto

---
+++ src/main/java/aulas/oo/part01/paradigma/exemplo001/exercicio/ExercicioCarro.java
@@ -0,0 +1,20 @@
+package aulas.oo.part01.paradigma.exemplo001.exercicio;
+
+public class ExercicioCarro {
+
+    private int qtdPessoa = 3;
+
+    public int getQtdPessoa(){
+        return qtdPessoa;
+    }
+
+    public String entraCarro(){
+        return "Pessoa entrando no carro";
+    }
+
+    public String saiCarro(){
+
+        return "Pessoa saindo do carro";
+    }
+
+}
---
+++ src/main/java/aulas/oo/part01/paradigma/exemplo001/exercicio/MetodoCarro.java
@@ -0,0 +1,12 @@
+package aulas.oo.part01.paradigma.exemplo001.exercicio;
+
+public class MetodoCarro {
+
+    public static void main(String[] args) {
+        ExercicioCarro carro = new ExercicioCarro();
+
+        System.out.println(carro.entraCarro());
+
+        System.out.println(carro.saiCarro());
+    }
+}
